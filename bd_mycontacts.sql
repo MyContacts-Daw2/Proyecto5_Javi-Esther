48e10a225d089603f1398757fcee583aa8566d8b
jAcebo <35202.joan23@fje.edu>
2017-01-27 17:10:47 +0100

    Ultima modificacion de la base de datos
    utlimos cambios.

--- bd_mycontacts.sql
+++ bd_mycontacts.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 26-01-2017 a las 17:02:26
+-- Tiempo de generación: 27-01-2017 a las 17:09:10
 -- Versión del servidor: 10.1.16-MariaDB
 -- Versión de PHP: 5.6.24
 
@@ -27,19 +27,21 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `tbl_perfil` (
-  `per_usuario` int(20) NOT NULL,
+  `usu_id` varchar(20) CHARACTER SET latin1 NOT NULL,
   `per_id` int(2) NOT NULL,
-  `per_nombre` varchar(15) NOT NULL,
-  `per_apellido1` varchar(15) NOT NULL,
-  `per_apellido2` varchar(15) NOT NULL,
-  `per_mail` varchar(50) NOT NULL,
+  `per_nombre` varchar(15) CHARACTER SET latin1 NOT NULL,
+  `per_apellido1` varchar(15) CHARACTER SET latin1 NOT NULL,
+  `per_apellido2` varchar(15) CHARACTER SET latin1 NOT NULL,
+  `per_mail` varchar(50) CHARACTER SET latin1 NOT NULL,
   `per_telf1` int(9) NOT NULL,
   `per_telf2` int(9) NOT NULL,
-  `per_direc1` varchar(50) NOT NULL,
-  `per_direc2` varchar(50) NOT NULL,
+  `per_direc1` varchar(50) CHARACTER SET latin1 NOT NULL,
+  `per_coord1` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `per_direc2` varchar(50) CHARACTER SET latin1 NOT NULL,
+  `per_coord2` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `per_coment` int(25) NOT NULL,
-  `per_foto` varchar(25) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `per_foto` varchar(25) CHARACTER SET latin1 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -48,9 +50,22 @@ CREATE TABLE `tbl_perfil` (
 --
 
 CREATE TABLE `tbl_usuario` (
-  `usu_usuario` int(20) NOT NULL,
-  `usu_password` int(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `usu_id` int(11) NOT NULL,
+  `usu_usuario` varchar(20) CHARACTER SET latin1 NOT NULL,
+  `usu_password` varchar(15) CHARACTER SET latin1 NOT NULL,
+  `usu_nombre` varchar(15) CHARACTER SET latin1 NOT NULL,
+  `usu_apellido1` varchar(15) CHARACTER SET latin1 NOT NULL,
+  `usu_apellido2` varchar(15) CHARACTER SET latin1 NOT NULL,
+  `usu_direc1` varchar(50) CHARACTER SET latin1 NOT NULL,
+  `usu_foto` varchar(25) CHARACTER SET latin1 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `tbl_usuario`
+--
+
+INSERT INTO `tbl_usuario` (`usu_id`, `usu_usuario`, `usu_password`, `usu_nombre`, `usu_apellido1`, `usu_apellido2`, `usu_direc1`, `usu_foto`) VALUES
+(0, 'david.marin', 'asdf', 'David', 'Marin', 'Salvador', 'calle horta ', '');
 
 --
 -- Índices para tablas volcadas
@@ -66,17 +81,17 @@ ALTER TABLE `tbl_perfil`
 -- Indices de la tabla `tbl_usuario`
 --
 ALTER TABLE `tbl_usuario`
-  ADD PRIMARY KEY (`usu_usuario`);
+  ADD PRIMARY KEY (`usu_id`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT de la tabla `tbl_usuario`
+-- AUTO_INCREMENT de la tabla `tbl_perfil`
 --
-ALTER TABLE `tbl_usuario`
-  MODIFY `usu_usuario` int(20) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `tbl_perfil`
+  MODIFY `per_id` int(2) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
